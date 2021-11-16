806b187cfb8bb7ed0eb80691ba23cc591b56ffd7
e1b18073 <e1b18073@st.oit.ac.jp>
2021-11-16 16:19:06 +0900

    welcome

---
+++ src/main/java/application/cardgame/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package application.cardgame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+
+  @GetMapping("/welcome")
+  public String sample21() {
+    return "welcome.html";
+  }
+
+}
