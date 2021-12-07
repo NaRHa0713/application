785afde5948df6203c426b5407376de79c393ba9
NaRHa0713 <e1b19104@oit.ac.jp>
2021-12-07 16:19:35 +0900

    非同期の仮実装

--- src/main/java/application/cardgame/CardgameApplication.java
+++ src/main/java/application/cardgame/CardgameApplication.java
@@ -2,12 +2,14 @@ package application.cardgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CardgameApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardgameApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CardgameApplication.class, args);
+  }
 
 }
